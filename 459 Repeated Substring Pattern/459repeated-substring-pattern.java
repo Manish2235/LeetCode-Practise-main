46113ca396487c3b780264046675a29f5a7c2868
manish80765 <manishlegeue8076@gmail.com>
2025-04-25 10:52:37 +0000

    Code challenge submission for question: 459 Repeated Substring Pattern

---
+++ 459 Repeated Substring Pattern/459repeated-substring-pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n=s.length();
+	    for(int i=n/2; i>=1; i--)
+	    {
+	    	if(n%i==0)
+	    	{
+	    		int time=n/i;
+
+		    	String pattern= s.substring(0, i);
+		    	String newstr="";
+		    	while(time-->0)
+		    	{
+		    		newstr+=pattern;
+		    	}
+		    	if(s.equals(newstr))
+		    	return true;
+	    	}
+	    }
+	   return false;
+    }
+}
