96e3e2b828e1e1c758ad6607d1745467bd319a79
manish80765 <manishlegeue8076@gmail.com>
2025-05-24 09:41:50 +0000

    Code challenge submission for question: 1679 Max Number of K-Sum Pairs

---
+++ 1679 Max Number of K-Sum Pairs/1679max-number-of-k-sum-pairs.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+      Arrays.sort(nums);
+	int a=0;
+	int left=0; 
+	int right=nums.length-1;
+	while(left<right)
+	{
+		if(nums[left]+nums[right]>k)
+		{
+			right--;
+		}
+		else if(nums[left]+nums[right]<k)
+		{
+			left++;
+		}
+		else
+		{
+			left++;
+			right--;
+			a++;
+		}
+	}
+	return a;   
+    }
+}
