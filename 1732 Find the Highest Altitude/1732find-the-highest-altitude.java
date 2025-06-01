9294b5dae80467a80b063dba5bb7b268a3dd00b0
manish80765 <manishlegeue8076@gmail.com>
2025-06-01 15:41:55 +0000

    Code challenge submission for question: 1732 Find the Highest Altitude

---
+++ 1732 Find the Highest Altitude/1732find-the-highest-altitude.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+         int n=gain.length;
+		 int current=0;
+		 int max=0; 
+		 for(int i=0; i<n; i++)
+		 {
+			 current=current+gain[i];
+			 max=Math.max(max, current);
+		 }
+		return max;
+    }
+}
