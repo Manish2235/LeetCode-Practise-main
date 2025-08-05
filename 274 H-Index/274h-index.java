c3de85d969aceebd72ffe28585b9ea654f0b5260
manish80765 <manishlegeue8076@gmail.com>
2025-08-05 15:34:32 +0000

    Code challenge submission for question: 274 H-Index

---
+++ 274 H-Index/274h-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+      
+		
+		Arrays.sort(citations);
+	        int n = citations.length;
+	        int h = 0;
+
+	        for (int i = 0; i < n; i++) {
+	            int papersWithAtLeastThisCitations = n - i;
+	            if (citations[i] >= papersWithAtLeastThisCitations) {
+	                h = papersWithAtLeastThisCitations;
+	                break;
+	            }
+	        }
+	        return h;	  
+    }
+}
