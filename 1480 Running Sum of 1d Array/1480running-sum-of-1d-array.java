734407cf8bb39303ad656e51bc2f4f555399bdf0
manish80765 <manishlegeue8076@gmail.com>
2025-10-29 17:31:24 +0000

    Code challenge submission for question: 1480 Running Sum of 1d Array

---
+++ 1480 Running Sum of 1d Array/1480running-sum-of-1d-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+       int n=nums.length;
+	for (int i=0; i<n-1; i++)
+	{
+		nums[i+1]=nums[i]+nums[i+1];
+	}
+	return nums;  
+    }
+}
