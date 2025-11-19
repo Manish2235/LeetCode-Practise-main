ca319ac77b6bce6c9e3b767d98f9101aa61ec2ab
Manish2235 <119679768+Manish2235@users.noreply.github.com>
2025-11-19 11:44:31 +0530

    Time: 0 ms (100%), Space: 42.5 MB (25.82%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    //bottom up Approch
+    public int rob(int[] nums) {  
+        int n= nums.length;
+        int[] t= new int[n+1];
+        Arrays.fill(t,0);
+        if(n==1) return nums[0];
+        t[0]=0;
+        t[1]=nums[0];
+        for(int i=2; i<=n; i++)
+        {
+            int steal= nums[i-1]+t[i-2];
+            int skip= t[i-1];
+            t[i] =Math.max(steal,skip);
+           
+        }
+         return t[n];
+    }
+}
