09dd9235c8f993ef0b666dca5624f5d462372316
Manish2235 <119679768+Manish2235@users.noreply.github.com>
2025-11-22 16:12:09 +0530

    Time: 1 ms (82.2%), Space: 43.1 MB (7.86%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -3,11 +3,10 @@ class Solution {
        boolean[] seen = new boolean[26];
         
         for (char c : sentence.toCharArray()) {
-            // Mark the corresponding index for each letter
+          
             seen[c - 'a'] = true;
         }
         
-        // Check if all letters have been seen
         for (boolean b : seen) {
             if (!b) {
                 return false;
