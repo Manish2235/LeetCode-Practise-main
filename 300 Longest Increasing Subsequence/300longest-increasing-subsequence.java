8bac3a0518ac35636851511eeff8fe277ac0d924
manish80765 <manishlegeue8076@gmail.com>
2025-11-24 04:45:56 +0000

    Code challenge submission for question: 300 Longest Increasing Subsequence

---
+++ 300 Longest Increasing Subsequence/300longest-increasing-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n= nums.length;
+        int[] t= new int[n];
+        int maxLIS=1;
+        Arrays.fill(t,1);
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                    t[i]=Math.max(t[i],t[j]+1);
+                    maxLIS=Math.max(maxLIS,t[i]);
+                }
+            }
+        }
+        return maxLIS;
+    }
+}
