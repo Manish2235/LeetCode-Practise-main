bdda5eafee900cd5b04d088a1bd52280bd9f73dc
manish80765 <manishlegeue8076@gmail.com>
2025-12-02 04:12:36 +0000

    Code challenge submission for question: 62 Unique Paths

---
+++ 62 Unique Paths/62unique-paths.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        long result = 1;
+        int totalMoves = m + n - 2; 
+        int movesDown = m - 1;    
+        for (int i = 1; i <= movesDown; i++) {
+            result = result * (totalMoves - movesDown + i) / i;
+        }
+        
+        return (int) result;
+    }
+}
