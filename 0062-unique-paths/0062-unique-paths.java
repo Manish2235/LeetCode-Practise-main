4aebdc4b673990463996c7b2f7d71a011d24c9d8
Manish2235 <119679768+Manish2235@users.noreply.github.com>
2025-12-02 09:42:41 +0530

    Time: 0 ms (100%), Space: 42 MB (40.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,10 +1,8 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         long result = 1;
-        int totalMoves = m + n - 2; // total steps robot must take
-        int movesDown = m - 1;     // choose positions for downward moves
-        
-        // We will compute C(totalMoves, movesDown)
+        int totalMoves = m + n - 2; 
+        int movesDown = m - 1;    
         for (int i = 1; i <= movesDown; i++) {
             result = result * (totalMoves - movesDown + i) / i;
         }
