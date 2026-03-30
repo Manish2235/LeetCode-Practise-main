b10690ea67a6ef43bdf9808168cf32cd72753638
Manish2235 <119679768+Manish2235@users.noreply.github.com>
2026-03-30 23:50:32 +0530

    Time: 3 ms (24.34%), Space: 43.3 MB (10.78%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,44 @@
+class Solution {
+   public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
+
+        String currentString = "";
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0'); // handle multi-digit
+            }
+
+            else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(currentString);
+
+                // reset
+                k = 0;
+                currentString = "";
+            }
+
+            else if (ch == ']') {
+                int repeat = countStack.pop();
+                String prevString = stringStack.pop();
+
+                StringBuilder temp = new StringBuilder(prevString);
+
+                for (int i = 0; i < repeat; i++) {
+                    temp.append(currentString);
+                }
+
+                currentString = temp.toString();
+            }
+
+            else {
+                currentString += ch;
+            }
+        }
+
+        return currentString;
+    }
+}
