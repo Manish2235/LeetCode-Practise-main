1757d7c370d71dcf115bf1436a3241e0d03f13b8
Manish2235 <119679768+Manish2235@users.noreply.github.com>
2026-08-26 14:14:27 +0530

    Time: 262 ms (77.55%), Space: 68.4 MB (22.78%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    scores_sorted = scores.sort_values('score', ascending=False)
+    scores_sorted['rank'] = scores_sorted['score'].rank(method='dense', ascending=False).astype(int)
+    result = scores_sorted[['score', 'rank']]
+    return result
